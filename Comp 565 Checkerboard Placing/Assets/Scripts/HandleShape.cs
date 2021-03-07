f32d0788160f7c52a3dd916f1ccec5b2ac9cc03b
Juan Davalos <jcharlied13@gmail.com>
2021-03-07 13:58:44 -0800

    welp did more than expected :p
    Added the primitives and swap. Shapes can be placed and the shapes are transparent.

---
+++ Comp 565 Checkerboard Placing/Assets/Scripts/HandleShape.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HandleShape : MonoBehaviour
+{
+    GameObject transparent;
+    Vector3 basePosition;
+    Vector3 hiddenPosition;
+    
+    
+    public GameObject cubeTransparent;
+    public GameObject sphereTransparent;
+    public GameObject capsuleTransparent;
+
+    public List<Button> buttons;
+
+    void Start()
+    {
+        // Create base and hidden positions
+        basePosition = new Vector3(0, 0.55f, 0);
+        hiddenPosition = new Vector3(0, -100, 0);
+
+        // Set current positions
+        transparent = cubeTransparent;
+        transparent.transform.position = basePosition;
+
+        // Set other transparent trackers as inactive
+        sphereTransparent.SetActive(false);
+        capsuleTransparent.SetActive(false);
+
+        // Set listeners when swap buttons detected
+        buttons[0].onClick.AddListener(() => Swapper(cubeTransparent));
+        buttons[1].onClick.AddListener(() => Swapper(sphereTransparent));
+        buttons[2].onClick.AddListener(() => Swapper(capsuleTransparent));
+    }
+
+    private void Swapper(GameObject go)
+    {
+        transparent.SetActive(false);
+        transparent.transform.position = hiddenPosition;
+        transparent = go;
+        transparent.SetActive(true);
+        transparent.transform.position = basePosition;
+    }
+ }
---
+++ Comp 565 Checkerboard Placing/Assets/Scripts/Platform.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{ 
+    public List<GameObject> gameObjects;
+
+    // Caching the current object to place
+    GameObject currentObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentObject = gameObjects[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonUp(0))  // check if left button is pressed
+        {
+            if (!currentObject.activeSelf)
+            {
+                SwapCurrentObject();
+            }
+            // take mouse position, convert from screen space to world space, do a raycast, store output of raycast into 
+            // hitInfo object ...
+
+            #region Screen To World
+            bool hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hitInfo);
+            if (hit)
+            {
+
+                #region HIDE
+                var userShape = CreateClone();
+
+                //cube.GetComponent<BoxCollider>().isTrigger = true;
+                //cube.GetComponent<Renderer>().material = blockMaterial;
+                #endregion
+
+                //cube.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + 0.5f, hitInfo.point.z);
+                #region HIDE
+                if (hitInfo.transform.CompareTag("Base"))
+                {
+                    userShape.transform.position = new Vector3(hitInfo.point.x, hitInfo.point.y + (0.5f), hitInfo.point.z);
+                }
+                #region HIDE
+                else
+                {
+                    if (hitInfo.normal == new Vector3(0, 0, 1)) // z+
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.transform.position.x, hitInfo.transform.position.y, hitInfo.point.z + (0.5f));
+                    }
+                    #region HIDE
+                    if (hitInfo.normal == new Vector3(1, 0, 0)) // x+
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.point.x + (0.5f), hitInfo.transform.position.y, hitInfo.transform.position.z);
+                    }
+                    if (hitInfo.normal == new Vector3(0, 1, 0)) // y+
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.transform.position.x, hitInfo.point.y + (0.5f), hitInfo.transform.position.z);
+                    }
+                    if (hitInfo.normal == new Vector3(0, 0, -1)) // z-
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.transform.position.x, hitInfo.transform.position.y, hitInfo.point.z - (0.5f));
+                    }
+                    if (hitInfo.normal == new Vector3(-1, 0, 0)) // x-
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.point.x - (0.5f), hitInfo.transform.position.y, hitInfo.transform.position.z);
+                    }
+                    if (hitInfo.normal == new Vector3(0, -1, 0)) // y-
+                    {
+                        userShape.transform.position = new Vector3(hitInfo.transform.position.x, hitInfo.point.y - (0.5f), hitInfo.transform.position.z);
+                    }
+                    #endregion
+                }
+                #endregion
+
+                //Debug.DrawRay(hitInfo.point, hitInfo.normal, Color.red, 2, false);
+                //Debug.Log(hitInfo.normal);
+                #endregion
+
+
+            }
+            else
+            {
+                Debug.Log(":p");
+            }
+            #endregion
+        }
+    }
+    
+    private void SwapCurrentObject()
+    {
+        // Iterate through the GOs and find the active one
+        foreach(var go in gameObjects)
+        {
+            if(go.activeSelf)
+            {
+                currentObject = go;
+                break;
+            }
+        }
+    }
+
+    private GameObject CreateClone()
+    {
+        GameObject go;
+        if (currentObject.CompareTag("Cube"))
+        {
+            go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        else if (currentObject.CompareTag("Sphere"))
+        {
+            go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        }
+        else
+        {
+            go = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        }
+
+        go.tag = "UserShape";
+        return go;
+    }
+}
