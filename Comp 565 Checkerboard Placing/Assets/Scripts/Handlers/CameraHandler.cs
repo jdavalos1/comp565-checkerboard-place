442e4384c18584d0060de0743c96765b18eb0da7
Juan Davalos <jcharlied13@gmail.com>
2021-03-07 23:59:20 -0800

    Can now rotate around a point left and right
    Currently still need to work on left and right sides of the code but will do slowly.

--- Comp 565 Checkerboard Placing/Assets/Scripts/Handlers/CameraHandler.cs
+++ Comp 565 Checkerboard Placing/Assets/Scripts/Handlers/CameraHandler.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CameraHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
 
+    static readonly Vector3 basePos = new Vector3(0, 0, 0);
     // Update is called once per frame
     void Update()
     {
-        
-    }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.RotateAround(basePos, Vector3.up, speed * Time.deltaTime);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.RotateAround(basePos, Vector3.down, speed * Time.deltaTime);
+        }
+/*        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.RotateAround(basePos, Vector3.left, speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.RotateAround(basePos, Vector3.right, speed * Time.deltaTime);
+        }
+*/    }
 }
